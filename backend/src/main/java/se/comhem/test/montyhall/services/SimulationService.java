ae992b51778970430ec6a35041ab7916a6530917
Anders <37899614+javaelev90@users.noreply.github.com>
2023-10-12 19:20:16 +0200

    Added runtime logging for the simulation jobs

--- backend/src/main/java/se/comhem/test/montyhall/services/SimulationService.java
+++ backend/src/main/java/se/comhem/test/montyhall/services/SimulationService.java
@@ -1,7 +1,10 @@
 package se.comhem.test.montyhall.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import se.comhem.test.montyhall.controllers.SimulationController;
 import se.comhem.test.montyhall.model.MontyHallSimulationJob;
 import se.comhem.test.montyhall.model.MontyHallSimulationResult;
 import se.comhem.test.montyhall.services.helpers.SimulationHelper;
@@ -15,6 +18,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class SimulationService implements ISimulationService{
+
+    private static final Logger logger = LoggerFactory.getLogger(SimulationService.class);
 
     private final SimulationHelper simulationHelper;
     private final Executor taskExecutor;
@@ -32,6 +37,7 @@ public class SimulationService implements ISimulationService{
 
         workPerThread.forEach(work -> futures.add(runSimulationPart(work, switchCase)));
 
+        long start = System.nanoTime();
         try {
             List<MontyHallSimulationResult> results = futures.stream()
                     .map(CompletableFuture::join)
@@ -39,6 +45,8 @@ public class SimulationService implements ISimulationService{
             return CompletableFuture.completedFuture(MontyHallSimulationResult.mergeResults(results));
         } catch (CompletionException | CancellationException ex){
             return CompletableFuture.failedFuture(ex);
+        } finally {
+            logger.info("Simulations took : {} ms",String.format("%.2f",(System.nanoTime() - start) / 1000000f));
         }
     }
 
