f99bcea7adf2f8252d84908d3a7d2007d682bd85
Anders <37899614+javaelev90@users.noreply.github.com>
2023-11-13 08:30:37 +0100

    Moved simulation form into sub component

--- frontend/src/Simulation/Simulation.jsx
+++ frontend/src/Simulation/Simulation.jsx
@@ -22,7 +22,6 @@ function Simulation() {
     const getSimulation = useCallback(async (event) => {
         event.preventDefault();
         setIsLoading(true);
-
         axios.get("simulation/monty-hall", { params : { numberOfSimulations : numberOfSimulations, switchCase : switchCase } })
             .then(res => {
                 if (res.status === 200) {
@@ -34,7 +33,7 @@ function Simulation() {
             })
             .finally(() => setIsLoading(false));
 
-    }, []);
+    }, [numberOfSimulations, switchCase, isLoading]);
 
     const handleSimulationChange = useCallback((event) => {
         let {value, min, max} = event.target;
@@ -48,29 +47,13 @@ function Simulation() {
 
     return (
         <div className="simulation columnAlignment centerAlignment simulation-container">
-            {/*<SimulationForm switchCase={switchCase}*/}
-            {/*                setSwitchCase={setSwitchCase}*/}
-            {/*                getSimulation={getSimulation}*/}
-            {/*                isLoading={isLoading}*/}
-            {/*                numberOfSimulations={numberOfSimulations}*/}
-            {/*                handleSimulationChange={handleSimulationChange}*/}
-            {/*/>*/}
-            <Form>
-                <Form.Group className="mb-3">
-                    <Form.Label>Number of simulations:</Form.Label>
-                    <Form.Control min="1" max="2147483647" type="number" required="required" placeholder="Enter number of simulations" value={numberOfSimulations} onChange={handleSimulationChange} />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>Switch case:</Form.Label>
-                    <Form.Check type="checkbox" value={switchCase} onChange={event => setSwitchCase(event.target.checked)}/>
-                </Form.Group>
-
-                <Form.Group className="rowAlignment leftAlignment">
-                    <Button className="btn btn-primary" type="submit" onClick={getSimulation} disabled={isLoading}>Simulate</Button>
-                    {isLoading && <Spinner animation="border"/>}
-                </Form.Group>
-            </Form>
+            <SimulationForm switchCase={switchCase}
+                            setSwitchCase={setSwitchCase}
+                            getSimulation={getSimulation}
+                            isLoading={isLoading}
+                            numberOfSimulations={numberOfSimulations}
+                            handleSimulationChange={handleSimulationChange}
+            />
             <div className="centerAlignment">
                 {simulationResult != null && <Doughnut data={chartData}/>}
             </div>
--- frontend/src/Simulation/form/Form.jsx
+++ frontend/src/Simulation/form/Form.jsx
@@ -1,27 +1,26 @@
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button";
-// import Spinner from "react-bootstrap/Spinner";
-// import React from "react";
-//
-//
-// export function SimulationForm(props){
-//
-//     return (
-//         <Form>
-//             <Form.Group className="mb-3">
-//                 <Form.Label>Number of simulations:</Form.Label>
-//                 <Form.Control min="1" max="2147483647" type="number" required="required" placeholder="Enter number of simulations" value={props.numberOfSimulations} onChange={props.handleSimulationChange} />
-//             </Form.Group>
-//
-//             <Form.Group className="mb-3">
-//                 <Form.Label>Switch case:</Form.Label>
-//                 <Form.Check type="checkbox" value={props.switchCase} onChange={event => props.setSwitchCase(event.target.checked)}/>
-//             </Form.Group>
-//
-//             <Form.Group className="rowAlignment leftAlignment">
-//                 <Button className="btn btn-primary" type="submit" onClick={props.getSimulation} disabled={props.isLoading}>Simulate</Button>
-//                 {props.isLoading && <Spinner animation="border"/>}
-//             </Form.Group>
-//         </Form>
-//     )
-// }
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import React from "react";
+
+
+export function SimulationForm(props){
+    return (
+        <Form>
+            <Form.Group className="mb-3">
+                <Form.Label>Number of simulations:</Form.Label>
+                <Form.Control min="1" max="2147483647" type="number" required="required" placeholder="Enter number of simulations" value={props.numberOfSimulations} onChange={props.handleSimulationChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Switch case:</Form.Label>
+                <Form.Check type="checkbox" value={props.switchCase} onChange={event => props.setSwitchCase(event.target.checked)}/>
+            </Form.Group>
+
+            <Form.Group className="rowAlignment leftAlignment">
+                <Button className="btn btn-primary" type="submit" onClick={props.getSimulation} disabled={props.isLoading}>Simulate</Button>
+                {props.isLoading && <Spinner animation="border"/>}
+            </Form.Group>
+        </Form>
+    )
+}
